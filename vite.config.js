5045ca1ed8192648da254353a448e6a0e5b4452c
Azizbekend <azizma3105@gmail.com>
2026-03-10 10:25:50 +0300

    we

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,32 @@
-import { defineConfig, loadEnv } from 'vite'  // Добавлен loadEnv
+import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
-    server: {
-      allowedHosts: true
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    allowedHosts: true
+  },
+  preview: {
+    allowedHosts: ['market.gsurso.ru'],
+    port: 5013,
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@landing': path.resolve(__dirname, './src/moduls/landing'),
+      '@shop': path.resolve(__dirname, './src/moduls/shop'),
+      '@admin': path.resolve(__dirname, './src/moduls/personal-account/admin'),
+      '@customer': path.resolve(__dirname, './src/moduls/personal-account/customer'),
+      '@supplier': path.resolve(__dirname, './src/moduls/personal-account/supplier'),
+      '@general': path.resolve(__dirname, './src/moduls/personal-account/general'),
+      '@common': path.resolve(__dirname, './src/moduls/personal-account/common'),
     },
-    preview: {
-      allowedHosts: ['market.gsurso.ru'],
-      port: 5013,
-      host: true,
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-        '@landing': path.resolve(__dirname, './src/moduls/landing'),
-        '@shop': path.resolve(__dirname, './src/moduls/shop'),
-        '@admin': path.resolve(__dirname, './src/moduls/personal-account/admin'),
-        '@customer': path.resolve(__dirname, './src/moduls/personal-account/customer'),
-        '@supplier': path.resolve(__dirname, './src/moduls/personal-account/supplier'),
-        '@general': path.resolve(__dirname, './src/moduls/personal-account/general'),
-        '@common': path.resolve(__dirname, './src/moduls/personal-account/common'),
-      },
-    },
-  }
+  },
 })
