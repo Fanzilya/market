fd6ca079958fcb873ca511a5692e6346e6510609
Azizbek <azizma3105@gmail.com>
2026-03-13 09:51:17 +0300

    Добавление всплывающих сообщение для регистрации

--- src/moduls/landing/pages/RegisterPage/index.tsx
+++ src/moduls/landing/pages/RegisterPage/index.tsx
@@ -53,7 +53,7 @@ export const RegisterPage = observer(() => {
                 styles={styles}
                 onClick={() => setFormData('roleName', Role.Supplier)}
                 isActive={formData.roleName === Role.Supplier}
-                description="ljkdjfklsjdlkfj"
+                description="Поставщик или Производитель"
               />
 
               <RegistrRoleButton
@@ -61,7 +61,7 @@ export const RegisterPage = observer(() => {
                 styles={styles}
                 onClick={() => setFormData('roleName', Role.Customer)}
                 isActive={formData.roleName === Role.Customer}
-                description="ljkdjfklsjdlkfj"
+                description="Проектная или Подрядная организация"
               />
 
             </div>
